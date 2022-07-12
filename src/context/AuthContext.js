79e3c2da29282f10d92e0c98dd3141f49889e385
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-07-12 10:41:32 -0300

    chore: initializes actions to sign up/in/out

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,30 @@ const authReducer = (state, action) => {
     }
 };
 
+const signup = (dispatch) => {
+    return ({email, password}) => {
+        // make api request to sign up with that email and password
+        // if we sign up, modify our state, and say that we are authenticated
+        // if signin up fails, we probably need to reflect an error message somewhere
+    }
+};
+
+const signin = (dispatch) => {
+    return ({email, password}) => {
+        // try to sign in
+        // handle success by updating state
+        // handle failure by showing error message
+    }
+};
+
+const signout = (dispatch) => {
+    return () => {
+        // somehow sign out
+    }
+};
+
 export const {Provider, Context} = createDataContext(
     authReducer,
-    {},
+    {signup, signin, signout},
     {isSignedIn: false}
 )
