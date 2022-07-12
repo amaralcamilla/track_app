e09374eb526fa3300b6ab4023e471d6d8671df5d
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-07-12 09:41:31 -0300

    chore: adds input props

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet} from "react-native";
 import {Text, Input, Button} from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // onChangeText={(newEmail) => setEmail(newEmail)}
+    // is the same as...
+    // onChangeText={setEmail}
+
     return (
         <View style={styles.container}>
             <Spacer>
                 <Text h3>Sign Up for Tracker</Text>
             </Spacer>
-            <Input label="Email"/>
-            <Spacer/>styling odds
-            <Input label="Password"/>
+            <Input
+                label="Email"
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            <Spacer/>
+            <Input
+                label="Password"
+                value={password}
+                onChangeText={setPassword}
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry
+            />
             <Spacer/>
             <Button title="Sign Up"/>
         </View>
