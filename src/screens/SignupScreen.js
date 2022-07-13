94c8d4fdbc3f8286f92bd0958853945fab196dd7
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-07-13 12:03:48 -0300

    refactor: extracts form logic

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,47 +1,28 @@
-import React, {useState, useContext} from 'react';
+import React, {useContext} from 'react';
 import {View, StyleSheet, TouchableOpacity} from "react-native";
-import {Text, Input, Button} from 'react-native-elements';
+import {Text} from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import {Context as AuthContext} from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
 
 const SignupScreen = ({navigation}) => {
     const {state, signup} = useContext(AuthContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
-    // onChangeText={(newEmail) => setEmail(newEmail)}
+    // onSubmit={({email, password}) => signup({email, password})}
     // is the same as...
-    // onChangeText={setEmail}
+    // onSubmit={signup}
 
     return (
         <View style={styles.container}>
-            <Spacer>
-                <Text h3>Sign Up for Tracker</Text>
-            </Spacer>
-            <Input
-                label="Email"
-                value={email}
-                onChangeText={setEmail}
-                autoCapitalize="none"
-                autoCorrect={false}
+            <AuthForm
+                headerText='Sign Up for Tracker'
+                errorMessage={state.errorMessage}
+                submitButtonText='Sign Up'
+                onSubmit={signup}
             />
-            <Spacer/>
-            <Input
-                label="Password"
-                value={password}
-                onChangeText={setPassword}
-                autoCapitalize="none"
-                autoCorrect={false}
-                secureTextEntry
-            />
-            {state.errorMessage ? (
-                <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-            ) : null}
-            <Spacer/>
-            <Button title="Sign Up" onPress={() => signup({email, password})}/>
             <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
                 <Spacer>
-                    <Text style={styles.link}>Already have an account? Sign in instead</Text>
+                    <Text style={styles.link}>Already have an account? Sign in instead.</Text>
                 </Spacer>
             </TouchableOpacity>
         </View>
@@ -59,12 +40,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         marginBottom: 220,
-    },
-    errorMessage: {
-        fontSize: 16,
-        color: 'red',
-        marginTop: 15,
-        marginLeft: 15,
     },
     link: {
         color: 'blue',
