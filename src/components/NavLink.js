2ee0c9ca4b55ca7e7ec073dada6bc6c1096ed89f
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-07-13 12:19:56 -0300

    refactor: creates a NavLink

---
+++ src/components/NavLink.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {StyleSheet, TouchableOpacity} from "react-native";
+import {withNavigation} from "react-navigation";
+import Spacer from './Spacer';
+import {Text} from "react-native-elements";
+
+const NavLink = ({navigation, text, routeName}) => {
+
+    return (
+        <TouchableOpacity onPress={() => navigation.navigate(routeName)}>
+            <Spacer>
+                <Text style={styles.link}>{text}</Text>
+            </Spacer>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    link: {
+        color: 'blue',
+    }
+})
+
+export default withNavigation(NavLink);
