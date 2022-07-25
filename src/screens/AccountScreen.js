9fce2a0680aa1c68e7157c388bceb982c86245fd
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-07-25 11:58:21 -0300

    chore: adds SafeAreaView

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -3,17 +3,18 @@ import {StyleSheet, Text} from "react-native";
 import {Button} from 'react-native-elements';
 import Spacer from "../components/Spacer";
 import {Context as AuthContext} from "../context/AuthContext";
+import {SafeAreaView} from "react-navigation";
 
 const AccountScreen = () => {
     const {signout} = useContext(AuthContext);
 
     return (
-        <>
+        <SafeAreaView forceInset={{top: 'always'}}>
             <Text style={{fontSize: 48}}>AccountScreen</Text>;
             <Spacer>
                 <Button title='Sign Out' onPress={signout}/>
             </Spacer>
-        </>
+        </SafeAreaView>
     )
 }
 
